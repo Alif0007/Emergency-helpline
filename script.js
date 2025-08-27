bb99924df338ced7a55a57c1d3e63d0e8823dfea
Al Amin Alif <alaminalif373@gmail.com>
2025-08-27 19:48:52 +0600

    heart button functionality added

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const heartAmount =document.getElementById('heart-amount')
+const coinAmount =document.getElementById('coinAmount')
+const card = document.getElementById('card')
+
+// Heart Count
+
+card.addEventListener('click', function(event){
+
+    if (event.target.classList.contains('heart-click')) {
+           
+    total = parseInt(heartAmount.innerText)
+    total++
+    heartAmount.innerText = total
+
+    } 
+})
+
+
+card.addEventListener('click', function(event){
+
+    if (event.target.classList.contains('btn-success')&& coinAmount.innerText >= 20 ) {
+    
+        console.log(card.children)
+        convertedCoin = parseInt(coinAmount.innerText)
+        coin = convertedCoin - 20
+        coinAmount.innerText = coin
+
+        alert(`call`)
+    
+
+    } 
+})
+
